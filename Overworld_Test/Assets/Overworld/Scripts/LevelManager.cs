51d9d49833afb32bde2272c4621f8b6bc0be7fd0
LoganDooley <logan_dooley@brown.edu>
2020-10-17 13:35:35 -0400

    Added Overworld file with gitignore

---
+++ Overworld_Test/Assets/Overworld/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public Animator transition;
+
+    public void LoadGameSelect(string name)
+    {
+        StartCoroutine(LoadLevel(name));
+    }
+
+    IEnumerator LoadLevel(string name)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(name);
+    }
+}
