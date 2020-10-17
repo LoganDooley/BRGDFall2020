1aff0179e088b3f57f49ce896658d23fe79c3d3b
LoganDooley <logan_dooley@brown.edu>
2020-10-17 15:43:56 -0400

    Added Dreamwood Font and scripting for buttons to change size upon hovering over them

---
+++ Overworld_Test/Assets/MouseOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseOver : MonoBehaviour
+{
+    public Button myButton;
+    public void OnMouseOver()
+    {
+        print("mouse is over button");
+        myButton.GetComponent<RectTransform>().localScale = new Vector3(1.2f, 1.2f, 1f); 
+    }
+    public void OnMouseExit()
+    {
+        print("mouse left button");
+        myButton.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+    }
+}
