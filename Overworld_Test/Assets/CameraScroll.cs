ba645758b8aabb44818a70b203403a58f1c8b2e0
LoganDooley <logan_dooley@brown.edu>
2020-10-24 16:43:07 -0400

    Added camera scrolling down to go to game select and back. Still working on optimized coefficients to make the Lerp look better

---
+++ Overworld_Test/Assets/CameraScroll.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScroll : MonoBehaviour
+{
+    private bool transition;
+    public float speed = 1f;
+    private float lerp = 0, duration = 1;
+    private Vector3 homePos, selectPos;
+    private int mode = 1;
+    private void Start()
+    {
+        transition = false;
+        homePos = transform.position;
+        selectPos = transform.position - new Vector3(0, 2 * transform.position.y, 0);
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("space") && transition == false)
+        {
+            print("space was hit");
+            transition = true;
+            mode = mode * -1;
+        }
+        if (transition & mode == -1)
+        {
+            lerp += speed*Time.deltaTime / (duration* (0.2f+Mathf.Abs(lerp - 0.5f)));
+            transform.position = Vector3.Lerp(homePos, selectPos, lerp);
+            if(transform.position == selectPos)
+            {
+                transition = false;
+                lerp = 0;
+            }
+        }
+        if(transition & mode == 1)
+        {
+            lerp += speed *Time.deltaTime / (duration* (0.2f+Mathf.Abs(lerp - 0.5f)));
+            transform.position = Vector3.Lerp(selectPos, homePos, lerp);
+            if (transform.position == homePos)
+            {
+                transition = false;
+                lerp = 0;
+            }
+        }
+    }
+}
